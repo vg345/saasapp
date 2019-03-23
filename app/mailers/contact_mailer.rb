72d69109ab7f2903b36c9638c9fa0deea99281b7
Vanshika Gulati <vanshikag345@gmail.com>
2019-03-23 06:36:34 +0000

    Added contact form mailer file

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,10 @@
+class ContactMailer < ActionMailer::Base
+  default to: 'vanshikag345@gmail.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
+end
