f88b0107766bde1dfdd17f1e1df52065f55944bd
Vanshika Gulati <vanshikag345@gmail.com>
2019-03-29 06:07:27 +0000

    Added profiles controller, new view, link to home page

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,10 @@
+class ProfilesController < ApplicationController
+  
+  # GET request to /users/:user_id/profile/new
+  def new
+    # Render profile creation form.
+  end
+  
+  def create
+  end
+end
