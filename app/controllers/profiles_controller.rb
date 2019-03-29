137fbf8c7f5b43cb2b35b3bc3028883b8f4f0f03
Vanshika Gulati <vanshikag345@gmail.com>
2019-03-29 06:54:10 +0000

    Added profiles create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,20 @@ class ProfilesController < ApplicationController
     @profile = Profile.new
   end
   
+  # POST request to /users/:user_id/profile
   def create
+    @user = User.find( params[:user_id] )
+    @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to root_path
+    else
+      render action: :new
+    end
   end
+  
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+    end
 end
