3b67ede7e0149501e1b4e4ec18f0863911a4e1e7
Vanshika Gulati <vanshikag345@gmail.com>
2019-03-29 06:39:48 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,6 +3,7 @@ class ProfilesController < ApplicationController
   # GET request to /users/:user_id/profile/new
   def new
     # Render profile creation form.
+    @profile = Profile.new
   end
   
   def create
